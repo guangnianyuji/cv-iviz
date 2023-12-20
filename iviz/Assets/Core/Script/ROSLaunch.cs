e5e0211e4ad0d5caaf253c6d77d5f093f1c1d51d
hugeBlack <wwkq03@outlook.com>
2023-12-20 23:20:02 +0800

    ros connect

---
+++ iviz/Assets/Core/Script/ROSLaunch.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Net.Sockets;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Iviz.Core
+{
+    public class ROSLaunch : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public TextMeshProUGUI textMeshPro;
+        public TextMeshProUGUI textMeshPro2;
+        public TMP_Dropdown dd;
+
+        public GameObject buildpanel;
+
+        private NetworkStream stream;
+
+        private TcpClient client;
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(ButtonClick);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        // void  ButtonClick()
+        // {
+
+        //     var httpClient = new HttpClient();
+        //     string url = textMeshPro.text; // Flask服务器地址
+        //         // 创建要发送的JSON数据
+
+        //     int id=dd.value;
+        //     Debug.Log(id);
+
+        //     string command="";
+        //     if(id==0)
+        //     {
+        //         command="roslaunch cartographer_ros demo_backpack_3d.launch bag_filename:=${HOME}/Downloads/b3-2016-04-05-14-14-00.bag";
+        //     }
+
+        //     var json = JsonSerializer.Serialize(new { command = command });
+        //     var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        //     try
+        //     {
+        //         // 发送POST请求
+        //         var response = await httpClient.PostAsync(url, content);
+
+        //         // 读取并输出响应内容
+        //         var responseContent = await response.Content.ReadAsStringAsync();
+        //         Console.WriteLine(responseContent);
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         // 错误处理
+        //         Console.WriteLine($"请求发送失败: {ex.Message}");
+        //     }
+        // }
+        void ButtonClick()
+        {
+            string server_str = textMeshPro.text.Trim((char)8203);
+
+            Debug.Log(server_str);
+            string port_str = textMeshPro2.text.Trim((char)8203);
+
+            int port = 0;
+            if (!int.TryParse(port_str, out port))
+            {
+                Debug.Log("Port Convert Failed;");
+                return;
+            }
+
+            Debug.Log(port);
+
+            int id = dd.value;
+            Debug.Log(id);
+            RosStartCommand command = new RosStartCommand();
+            RosConnector.get().setAddress(server_str, port);
+            if (id == 0)
+            {
+                //"roslaunch cartographer_ros demo_backpack_3d.launch bag_filename:=${HOME}/Downloads/b3-2016-04-05-14-14-00.bag";
+                command.bagName = "b3-2016-04-05-14-14-00";
+            }
+
+            Task a = Task.Run(async () =>
+            {
+                await RosConnector.get().sendCommandAsync(command);
+                
+            });
+            buildpanel.SetActive(false);
+
+
+
+
+        }
+    }
+}
+
---
+++ iviz/Assets/Core/Script/RosCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Iviz.Core
+{
+    public class RosCommand
+    {
+        public string command;
+    }
+
+    public class RosStartCommand : RosCommand
+    {
+        public string bagName;
+
+        public RosStartCommand()
+        {
+            command = "start";
+        }
+    }
+
+    public class RosConnector
+    {
+        private static RosConnector instance;
+        private string serverAddress;
+        private HttpClient httpClient;
+        public static RosConnector get()
+        {
+            if (instance == null)
+                instance = new RosConnector();
+            return instance;
+        }
+
+        public void setAddress(string hostName, int port)
+        {
+            serverAddress = "http://" + hostName + ":" + port;
+            httpClient = new()
+            {
+                BaseAddress = new Uri(serverAddress),
+            };
+        }
+
+        public async Task sendCommandAsync(RosCommand command)
+        {
+            string commandJson = JsonConvert.SerializeObject(command, Formatting.Indented);
+            StringContent content = new StringContent(commandJson, Encoding.UTF8, "application/json");
+
+
+
+            using HttpResponseMessage response = await httpClient.PostAsync("command",content);
+            response.EnsureSuccessStatusCode();
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Debug.Log($"{jsonResponse}\n");
+        }
+
+
+    }
+}
